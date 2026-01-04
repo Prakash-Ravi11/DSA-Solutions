52c8c947f4c0b3f114457e9e096e10a495ee6ba4
R.R.PRAKASH RAVI <185219267+Prakash-Ravi11@users.noreply.github.com>
2026-01-04 23:27:48 +0530

    Update remove_duplicates_sorted.c

--- Arrays/remove_duplicates_sorted.c
+++ Arrays/remove_duplicates_sorted.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n; scanf("%d",&n);
+    int a[n], j=0;
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    for(int i=0;i<n;i++)
+        if(i==0 || a[i]!=a[i-1]) a[j++]=a[i];
+    for(int i=0;i<j;i++) printf("%d ",a[i]);
+    return 0;
+}
 
