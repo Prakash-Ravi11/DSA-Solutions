03fa7c233383f4495c27f6fc601f70ffa5d4b264
R.R.PRAKASH RAVI <185219267+Prakash-Ravi11@users.noreply.github.com>
2026-01-04 23:28:06 +0530

    Update linear_search.c

--- Arrays/linear_search.c
+++ Arrays/linear_search.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+int main(){
+    int n,x; scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    scanf("%d",&x);
+    for(int i=0;i<n;i++)
+        if(a[i]==x){ printf("Found"); return 0; }
+    printf("Not Found");
+    return 0;
+}
 
