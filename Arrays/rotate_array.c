632322897b7f8dccd97a6dab8c4942dd9ce1c8d3
R.R.PRAKASH RAVI <185219267+Prakash-Ravi11@users.noreply.github.com>
2026-01-04 23:27:28 +0530

    Add array rotation functionality
    Implement array rotation to the right by k positions.

--- Arrays/rotate_array.c
+++ Arrays/rotate_array.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main(){
+    int n,k; scanf("%d%d",&n,&k);
+    int a[n];
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    for(int r=0;r<k;r++){
+        int last=a[n-1];
+        for(int i=n-1;i>0;i--) a[i]=a[i-1];
+        a[0]=last;
+    }
+    for(int i=0;i<n;i++) printf("%d ",a[i]);
+    return 0;
+}
 
