1ffd1dbe6e12281c7ed6df2866c4785dc0ec455a
R.R.PRAKASH RAVI <185219267+Prakash-Ravi11@users.noreply.github.com>
2026-01-04 23:28:42 +0530

    Update second_largest.c

--- Arrays/second_largest.c
+++ Arrays/second_largest.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+int main(){
+    int n; scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    int max=-1e9, sec=-1e9;
+    for(int i=0;i<n;i++){
+        if(a[i]>max){ sec=max; max=a[i]; }
+        else if(a[i]>sec && a[i]!=max) sec=a[i];
+    }
+    printf("%d",sec);
+    return 0;
+}
